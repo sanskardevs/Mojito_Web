006737e5236b105db269f43f0efb4e5f48dcfc66
Sanskar Amle <25amlesp@rbunagpur.in>
2026-08-11 23:52:20 +0530

    project complete

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,7 +1,7 @@
-import { featureLists, goodLists } from "../../constants/index.js";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import { featureLists, goodLists } from "../../constants/index.js";
 
 const Art = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -20,17 +20,13 @@ const Art = () => {
     });
 
     maskTimeline
-      .to(".will-fade", {
-        opacity: 0,
-        stagger: 0.2,
-        ease: "power1.inOut",
-      })
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
       .to(".masked-img", {
         scale: 1.3,
         maskPosition: "center",
         maskSize: "400%",
         duration: 1,
-        ease: "power1.inOut",
+        ease: "power1.inOut ",
       })
       .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
   });
@@ -49,6 +45,7 @@ const Art = () => {
               </li>
             ))}
           </ul>
+
           <div className="cocktail-img">
             <img
               src="/images/under-img.jpg"
@@ -56,6 +53,7 @@ const Art = () => {
               className="abs-center masked-img size-full object-contain"
             />
           </div>
+
           <ul className="space-y-4 will-fade">
             {featureLists.map((feature, index) => (
               <li key={index} className="flex items-center justify-start gap-2">
@@ -64,20 +62,20 @@ const Art = () => {
               </li>
             ))}
           </ul>
-          <div className="masked-container">
-            <h2 className="will-fade">Skip Worthy Perfection</h2>
-            <div id="masked-content">
-              <h3>Made with Craft,Poured with Passion</h3>
-              <p>
-                This isn't just a drink.It's a carefully crafted moment made
-                just for you.
-              </p>
-            </div>
+        </div>
+
+        <div className="masked-container">
+          <h2 className="will-fade">Sip-Worthy Perfection</h2>
+          <div id="masked-content">
+            <h3>Made with Craft, Poured with Passion</h3>
+            <p>
+              This isn’t just a drink. It’s a carefully crafted moment made just
+              for you.
+            </p>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Art;
